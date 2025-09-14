9499781975346bb062a8ec06a8e3423d2162f155
Eliabr Rodrigues <eliabe859@gmail.com>
2025-09-14 01:34:51 -0300

    chore: fix the files

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -2,7 +2,7 @@ export default function Welcome() {
   return (
     <div className="bg-[url('/assets/backgraundWelcome.jpg')] bg-cover bg-center h-screen w-full relative">
       <div className="text-white text-center absolute bottom-28 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-8xl font-avoda">Isabelly & Matheus</h1>
+        <h1 className="text-8xl font-avoda">Izabelly & Matheus</h1>
         <span className="text-3xl">16/11/25</span>
       </div>
     </div>
