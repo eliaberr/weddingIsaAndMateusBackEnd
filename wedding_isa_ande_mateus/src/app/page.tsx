a7505213c3e30c96cbe3ab6abc4ad681b0245e18
Eliabr Rodrigues <eliabe859@gmail.com>
2025-09-14 01:27:40 -0300

    feat: created the projects add the configuration and created the welcome

---
+++ wedding_isa_ande_mateus/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Welcome from "./pages/home";
+
+export default function Home() {
+  return (
+    <div>
+      <Welcome/>
+    </div>
+  );
+}
---
+++ wedding_isa_ande_mateus/src/app/pages/home.tsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+  return (
+    <div className="bg-[url('/assets/backgraundWelcome.jpg')] bg-cover bg-center h-screen w-full relative">
+      <div className="text-white text-center absolute bottom-28 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-8xl font-avoda">Isabelly & Matheus</h1>
+        <span className="text-3xl">16/11/25</span>
+      </div>
+    </div>
+  );
+}
